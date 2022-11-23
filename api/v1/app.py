5a807eafd118327e0485c17accfef0b6266d40b6
bbrown585 <brandon_scott585@yahoo.com>
2022-11-22 17:58:27 -0800

    index and app update

--- api/v1/app.py
+++ api/v1/app.py
@@ -37,13 +37,7 @@ app.config['SWAGGER'] = {
 
 Swagger(app)
 
-
-if __name__ == "__main__":
-    """ Main Function """
-    host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
+if __name__ == '__main__':
+    host = environ.get('HBNB_API_HOST', default='0.0.0.0')
+    port = environ.get('HBNB_API_PORT', default='5000')
     app.run(host=host, port=port, threaded=True)
