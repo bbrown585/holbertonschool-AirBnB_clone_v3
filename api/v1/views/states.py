d4c44ba4e248093008119eb88dbed2b7dd22638e
bbrown585 <brandon_scott585@yahoo.com>
2022-11-28 13:45:25 -0800

    updating states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -20,8 +20,8 @@ def get_states():
     return jsonify(list_states)
 
 
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/state/get_id_state.yml', methods=['get'])
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+@swag_from('documentation/state/get_id_state.yml', methods=['GET'])
 def get_state(state_id):
     """ Retrieves a specific State """
     state = storage.get(State, state_id)
